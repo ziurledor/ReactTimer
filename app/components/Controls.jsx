263b3b5e009630c77bfe823345f71a601284c07e
Rodel Ruiz <Rodel Ruiz>
2016-11-23 23:48:35 +0300

    Update status to stop when count reaches zero

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -10,6 +10,9 @@ var Controls = React.createClass({
         this.props.onStatusChange(newStatus);
       }
   },
+  componentWillReceiveProps: function (newProps) {
+    console.log('componentWillReceiveProps', newProps.countdownStatus);
+  },
   render: function () {
     var {countdownStatus} = this.props;
     var renderStartStopButton = () => {
--- public/bundle.js
+++ public/bundle.js
@@ -25633,6 +25633,10 @@
 	      }
 	    }
 	  },
+	  componentWillUnmount: function componentWillUnmount() {
+	    clearInterval(this.timer);
+	    this.timer = undefined;
+	  },
 	  startTimer: function startTimer() {
 	    var _this = this;
 
@@ -25641,6 +25645,9 @@
 	      _this.setState({
 	        count: newCount >= 0 ? newCount : 0
 	      });
+	      if (newCount === 0) {
+	        _this.setState({ countdownStatus: 'stopped' });
+	      }
 	    }, 1000);
 	  },
 	  handleSetCountdown: function handleSetCountdown(seconds) {
@@ -25786,6 +25793,9 @@
 	    return function () {
 	      _this.props.onStatusChange(newStatus);
 	    };
+	  },
+	  componentWillReceiveProps: function componentWillReceiveProps(newProps) {
+	    console.log('componentWillReceiveProps', newProps.countdownStatus);
 	  },
 	  render: function render() {
 	    var _this2 = this;
